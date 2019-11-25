19fa1ebab1f910439566383b5dabd489add52998
Willi Mentzel <willi.mentzel@gmail.com>
2019-11-25 23:21:03 +0100

    Remove Netbeans' licence header template

--- src/main/java/com/randomlychosenbytes/jlocker/dialogs/DisplayGraphDialog.java
+++ src/main/java/com/randomlychosenbytes/jlocker/dialogs/DisplayGraphDialog.java
@@ -10,12 +10,6 @@ import org.jgrapht.graph.SimpleWeightedGraph;
 import javax.swing.*;
 import java.awt.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * @author Willi
  */
