47347b5de4baf8487531651f534c7fad2396f3a1
Willi Mentzel <willi.mentzel@gmail.com>
2022-09-04 02:27:26 +0200

    Move everything to com.randomlychosenbytes.jlocker

---
+++ src/main/java/com/randomlychosenbytes/jlocker/nonabstractreps/Entity.java
@@ -0,0 +1,45 @@
+package com.randomlychosenbytes.jlocker.nonabstractreps;
+
+public class Entity implements java.io.Serializable {
+    /**
+     * If the object is manipulated another serialVersionUID will be assigned
+     * by the compiler, even for minor changes. To avoid that it is set
+     * by the programmer.
+     */
+    private static final long serialVersionUID = 8606236005570624210L;
+
+    public Entity() {
+    }
+
+    protected String sName;
+
+    /* *************************************************************************
+     * Setter
+     **************************************************************************/
+    public void setName(String newname) {
+        sName = newname;
+    }
+
+    /**
+     * XMLEncoder
+     *
+     * @param newname
+     */
+    public void setSName(String newname) {
+        sName = newname;
+    }
+
+    /* *************************************************************************
+     * Getter
+     **************************************************************************/
+    public String getName() {
+        return sName;
+    }
+
+    /**
+     * XMLEncoder
+     */
+    public String getSName() {
+        return sName;
+    }
+}
